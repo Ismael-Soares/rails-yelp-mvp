85c1ef256cd715de10aa97ad68dc63930385904d
Ismael-Soares bellsoares3@gmail.com <bellsoares3@gmail.com>
2023-11-15 16:23:43 -0300

    MVP do YELP

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,10 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+puts 'Criando 5 Restaurantes'
+
+Restaurant.create!(name: 'Rangofood', address: 'Rua: de trás', category: 'chinese')
+Restaurant.create!(name: 'bombar', address: 'Rua: da frente', category: 'chinese')
+Restaurant.create!(name: 'Bar baridade', address: 'Av: Principal', category: 'italian')
+Restaurant.create!(name: 'Tô em casa', address: 'Rua: perdida', category: 'chinese')
+Restaurant.create!(name: 'Esquina', address: 'Rua: jericó', category: 'chinese')
